68218e57ab99e6e8ca96d6f3241207e5afea95f1
alumno <alumno@utnfra.com.ar>
2019-08-27 21:53:29 -0300

    hey

---
+++ Clase3Laboratorio/Ejercicios17-18/Ejercicio17/Class1.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio17
+{
+  class Boligrafo
+  {
+    short cantidadTintaMaxima = 100;
+    private ConsoleColor color;
+    private short tinta;
+    public ConsoleColor GetColor()
+    {
+
+    }
+    public short GetTinta()
+    {
+
+    }
+    private void SetTinta(short tinta1)
+    {
+      this.tinta = tinta1;
+    }
+    public void Recargar()
+    {
+
+    }
+    public bool Pintar(short gasto, out string dibujo)
+    {
+
+    }
+  }
+}
---
+++ Clase3Laboratorio/Ejercicios17-18/Ejercicio17/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio17
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Boligrafo b1 = new Boligrafo();
+    }
+  }
+}
