0a8023daa570864e8700cf988880f3419d3ead70
alumno <alumno@utnfra.com.ar>
2019-09-30 19:00:54 -0300

    hoy

---
+++ Clase12Programacion/Excepciones/ClassLibrary1/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Entidades
+{
+    public abstract class Persona
+    {
+        public int Cuil { get; set; }
+        public string Nombre { get; set; }
+        public Persona(string nombre, int cuil)
+        {
+            this.Cuil = cuil;
+            this.Nombre = nombre;
+        }
+
+        public virtual string Mostrar()
+        {
+            return "nom:"+  this.Nombre + " cuil:" + this.Cuil;
+        }
+    }
+}
---
+++ Clase12Programacion/Excepciones/WindowsFormsApp1/FrmAumentos.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FrmAumentos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAumentar = new System.Windows.Forms.Button();
+            this.txtPorcentaje = new System.Windows.Forms.TextBox();
+            this.lstNomina = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnAumentar
+            // 
+            this.btnAumentar.Location = new System.Drawing.Point(323, 41);
+            this.btnAumentar.Name = "btnAumentar";
+            this.btnAumentar.Size = new System.Drawing.Size(75, 23);
+            this.btnAumentar.TabIndex = 0;
+            this.btnAumentar.Text = "Aumentar";
+            this.btnAumentar.UseVisualStyleBackColor = true;
+            this.btnAumentar.Click += new System.EventHandler(this.btnAumentar_Click);
+            // 
+            // txtPorcentaje
+            // 
+            this.txtPorcentaje.Location = new System.Drawing.Point(12, 41);
+            this.txtPorcentaje.Name = "txtPorcentaje";
+            this.txtPorcentaje.Size = new System.Drawing.Size(132, 20);
+            this.txtPorcentaje.TabIndex = 2;
+            // 
+            // lstNomina
+            // 
+            this.lstNomina.FormattingEnabled = true;
+            this.lstNomina.Location = new System.Drawing.Point(12, 82);
+            this.lstNomina.Name = "lstNomina";
+            this.lstNomina.Size = new System.Drawing.Size(386, 251);
+            this.lstNomina.TabIndex = 3;
+            // 
+            // FrmAumentos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 362);
+            this.Controls.Add(this.lstNomina);
+            this.Controls.Add(this.txtPorcentaje);
+            this.Controls.Add(this.btnAumentar);
+            this.Name = "FrmAumentos";
+            this.Text = "FrmAumentos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAumentar;
+        private System.Windows.Forms.TextBox txtPorcentaje;
+        private System.Windows.Forms.ListBox lstNomina;
+    }
+}
