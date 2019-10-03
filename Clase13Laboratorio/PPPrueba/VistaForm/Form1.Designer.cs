a7db6e57785a840eb542e9c2de8c604db66d72e1
DiegoChirdo <wchirdo@gmail.com>
2019-10-03 01:31:40 -0300

    repasoprimerparcialhecho

---
+++ Clase13Laboratorio/PPPrueba/VistaForm/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace VistaForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.numEdad = new System.Windows.Forms.NumericUpDown();
+            this.numDNI = new System.Windows.Forms.NumericUpDown();
+            this.numExp = new System.Windows.Forms.NumericUpDown();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.btnValidar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numEdad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDNI)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numExp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Edad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "DNI";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Experiencia";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(87, 23);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(151, 20);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(87, 55);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(151, 20);
+            this.txtApellido.TabIndex = 6;
+            // 
+            // numEdad
+            // 
+            this.numEdad.Location = new System.Drawing.Point(87, 88);
+            this.numEdad.Name = "numEdad";
+            this.numEdad.Size = new System.Drawing.Size(145, 20);
+            this.numEdad.TabIndex = 7;
+            // 
+            // numDNI
+            // 
+            this.numDNI.Location = new System.Drawing.Point(87, 122);
+            this.numDNI.Maximum = new decimal(new int[] {
+            999999,
+            0,
+            0,
+            0});
+            this.numDNI.Name = "numDNI";
+            this.numDNI.Size = new System.Drawing.Size(145, 20);
+            this.numDNI.TabIndex = 8;
+            // 
+            // numExp
+            // 
+            this.numExp.Location = new System.Drawing.Point(87, 156);
+            this.numExp.Name = "numExp";
+            this.numExp.Size = new System.Drawing.Size(145, 20);
+            this.numExp.TabIndex = 9;
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(22, 237);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(121, 44);
+            this.btnCrear.TabIndex = 10;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.BtnCrear_Click);
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Location = new System.Drawing.Point(207, 237);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(121, 44);
+            this.btnValidar.TabIndex = 11;
+            this.btnValidar.Text = "Validar";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.BtnValidar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 304);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.numExp);
+            this.Controls.Add(this.numDNI);
+            this.Controls.Add(this.numEdad);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "VistaForm";
+            ((System.ComponentModel.ISupportInitialize)(this.numEdad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDNI)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numExp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.NumericUpDown numEdad;
+        private System.Windows.Forms.NumericUpDown numDNI;
+        private System.Windows.Forms.NumericUpDown numExp;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Button btnValidar;
+    }
+}
+
---
+++ Clase13Laboratorio/PPPrueba/VistaForm/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace VistaForm
+{
+    public partial class Form1 : Form
+    {
+        private DirectorTecnico dt = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCrear_Click(object sender, EventArgs e)
+        {
+            dt = new DirectorTecnico(txtNombre.Text, txtApellido.Text, (int)numEdad.Value, (int)numDNI.Value, (int)numExp.Value);
+            MessageBox.Show("¡Se ha creado el DT!");
+        }
+
+        private void BtnValidar_Click(object sender, EventArgs e)
+        {
+            if (dt == null)
+                MessageBox.Show("Aún no se ha creado el DT");
+            if (dt.ValidarAptitud() == true)
+                MessageBox.Show("El DT es apto");
+            else if (dt.ValidarAptitud() == false)
+                MessageBox.Show("El DT no es apto");
+            txtNombre.Text = "";
+            txtApellido.Text = "";
+            numDNI.Value = 0;
+            numEdad.Value = 0;
+            numExp.Value = 0;
+        }
+    }
+}
