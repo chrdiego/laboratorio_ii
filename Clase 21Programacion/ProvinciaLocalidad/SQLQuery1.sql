06afc04738fbd99b4b82c36164b37e80523b95b7
alumno <alumno@utnfra.com.ar>
2019-11-04 21:52:58 -0300

    Hoy

--- Clase 21Programacion/ProvinciaLocalidad/SQLQuery1.sql
+++ Clase 21Programacion/ProvinciaLocalidad/SQLQuery1.sql
@@ -1,3 +1,14 @@
-select * from Provincia
 
-select * from Localidad
+select * from Provincia A inner join Localidad B on A.id = B.idProvincia
+
+select A.nombre.Count(1)
+
+--insert into Provincia(id, nombre) values (66 ,'jhgfjh')
+
+delete Provincia where id = 2
+
+select * from Localidad where id = 2
+
+select * from Provincia order by id asc
+
+group by A.nombre
